754711b4338eb998fbf15349c7382bf1a108467a
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2017-01-19 23:25:52 -0200

    Project Created!

---
+++ MovieList/MovieList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Leonardo Saragiotto on 1/19/17.
+//  Copyright © 2017 Leonardo Saragiotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
