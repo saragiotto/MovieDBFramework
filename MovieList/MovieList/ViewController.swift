c5cb5770288bb4ac749f225f5ae22af6854708fb
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2017-01-20 00:07:48 -0200

    Add Alamofire dependency

--- MovieList/MovieList/ViewController.swift
+++ MovieList/MovieList/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        Alamofire.request("https://api.themoviedb.org/3/genre/movie/list?api_key=1f54bd990f1cdfb230adb312546d765d&language=en-US").responseJSON { response in
+            debugPrint(response)
+            
+            if let json = response.result.value {
+                print("JSON: \(json)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
